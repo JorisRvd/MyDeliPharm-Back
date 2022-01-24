f7404f1a89147dc80c4f102bee5abb477ffae858
Jorisrvd <Jranely@gmail.com>
2022-01-24 11:48:50 +0100

    rererreeupdate

--- src/Entity/Dispensary.php
+++ src/Entity/Dispensary.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\DispensaryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -14,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Dispensary
 {
     /**
-     * @dORM\Id
+     * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
